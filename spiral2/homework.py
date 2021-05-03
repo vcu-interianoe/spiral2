9a6e996b212c801af009213754905477a7eb3314
eddy <interianoe@vcu.edu>
2021-05-03 16:20:55 -0400

    updates

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,16 @@
 def spiralize(size, n=1):
-    """ Your code goes somewhere in here"""
-    return_value = n
+    x = n
+    y = 2
+    current_value = n
+    reset = 0
+
+    while current_value < pow(size, n) + n:
+        current_value += y
+        x += current_value
+        reset += 1
+        if reset == 4
+            y += 2
+            reset = 0
     return return_value
+
+print(spiralize(501,15))
