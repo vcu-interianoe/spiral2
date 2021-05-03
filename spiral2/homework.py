74e79c1acc288f2a9b976c8582a2595732ea38f4
eddy <interianoe@vcu.edu>
2021-05-03 16:35:18 -0400

    more updates

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -4,11 +4,11 @@ def spiralize(size, n=1):
     current_value = n
     reset = 0
 
-    while current_value < pow(size, n) + n-1:
+    while current_value < pow(size, 2) + n - 1:
         current_value += y
         x += current_value
         reset += 1
-        if reset = 4
+        if reset == 4
             y += 2
             reset = 0
     return current_value
